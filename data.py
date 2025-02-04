1ba48dbc49d6ba888f6d5634f4dba98e54a82f37
weiy-1 <weiyuanyan1126@gmail.com>
2025-02-04 10:04:31 -0500

    coomiited

---
+++ data.py
@@ -0,0 +1,27 @@
+#Strings arre for representing characters, names, words, etc.
+name= "divid"
+#integers represent whole numbers
+age: 14
+#Floats represents decimals
+wallet: 5.45
+#booleans represents true/false, used in evaluations
+graduated = False
+
+
+def add(x,y):
+    print(x + y)
+#input asks the user a question and stores their response as a value
+#bill = float(input("How much did the bill cost?"))
+#print(type(bill))
+#add(25, bill)
+
+
+#lists
+students = ["Joanna", "Divid", "Deivid", "other Deivid", "Etan"]
+#this is similar to saying for i in range(5): print(students[i])
+""" print(students[2]) """
+""" for student in students:
+    print(student) """
+moneys = [1,2,3,4,5,6]
+total: 0
+for money in moneys:
