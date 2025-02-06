5339d98fa216f03bea4d24f960c0da04e56b2c45
weiy-1 <weiyuanyan1126@gmail.com>
2025-02-06 10:17:28 -0500

    completed tipcaluator

--- data.py
+++ data.py
@@ -1,5 +1,5 @@
 #Strings are for representing characters, names, words, etc.
-name= "divid"
+name = "divid"
 #integers represent whole numbers
 age: 14
 #Floats represents decimals
@@ -24,15 +24,30 @@ graduated = False
     print(student) """
 """ moneys = [1,2,3,4,5,6]
 total: 0
-for money in moneys:
- """
+for money in moneys: """
+
 
 #tip calculator:
-def add(x,y):
+""" def add(x,y):
     print(x + y)
 bill = float(input("How much does the bill cost?"))
 print(type(bill))
 tip = float(input("How much is the tip?"))
 print(type(tip))
 add(bill,tip)
+total = (float(bill + tip))
+print("Total:", total) """
+
+
+#prints all numbers inside the brackets
+""" values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(i) """
+
+#prints only the 3rd(2) and 6th(5) number
+""" values = [1,2.23,5,7,2,30,15]
+print(values[2])
+print(values[5]) """
+
 
