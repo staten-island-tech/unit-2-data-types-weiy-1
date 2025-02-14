c11efde4d887ff351e6e1529ef8fd50fa187f77d
weiy-1 <weiyuanyan1126@gmail.com>
2025-02-14 11:08:46 -0500

    working number guess

--- gcprojs.py
+++ gcprojs.py
@@ -1,5 +1,5 @@
-""" #mad libs
-name = (input("Input a celebrity's name: "))
+#mad libs
+""" name = (input("Input a celebrity's name: "))
 print(type(name))
 food = (input("Input a food: "))
 print(type(food))
@@ -15,13 +15,31 @@ name_2 = (input("Input another name: "))
 print(type(name_2))
 verb_2 = (input("Input another verb: "))
 print(type(verb_2))
-print("Today,", name, "tried to eat some", food, "but then someone came to his doorstep and", verb, "his house. It was very", adjective, "and", name, "started running until the house was 14.628 miles away. This person had chased", name, "since", name, "took 451", noun, "from", noun_2, "yesterday.", name_2, "saw", name, "running and wanted to also", verb_2, name, "for stealing those 451", noun,".") """
+print("Today,", name, "tried to eat some", food, "but then someone came to his doorstep and", verb, "his house. It was very", adjective, "and", name, "started running until the house was 14.628 miles away. This person had chased", name, "since", name, "took 451", noun, "from", noun_2, "yesterday.", name_2, "saw", name, "running and wanted to", verb_2, name, "for stealing those 451", noun,".")
+ """
 
-
-number = (int(input("Input a number:")))
-print(type(number))
-
-if number % 2 == 0:
-    print("This number is even")
+#random numb generator
+import random
+numbers = ['1','2','3','4','5','6','7','8','9','10']
+random_number = random.choice(numbers)
+guessed_number = input("Guess the number:")
+if guessed_number < random_number:  
+    print(input("Higher, try again:"))
+elif guessed_number > random_number:
+    print(input("Lower, try again:"))
 else:
-    print("This number is odd")
+    print("You guessed correctly")
+
+
+
+""" def grade(score):
+    if score >= 92:
+        print("A")
+    elif score >= 82:
+        print("B")
+    elif score >= 72:
+        print("C")
+    else:
+        print("F")
+x = int(input("What's the score?"))
+grade(x) """
